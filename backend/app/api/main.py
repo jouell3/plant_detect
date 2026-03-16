30222482d56c2dcb60d596957a45c94f381dded2
Jimmy Ouellet <jimmy.ouellet@gmail.com>
2026-03-16 21:44:30 +0100

    select tool streamlit added

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, File, UploadFile
+#from herbs_detection.load_data import load_model
 
 api = FastAPI()
+
+#model = load_model()
 
 @api.get("/")
 def root():
     return {"message": "Hello World"}
 
-api.post("/predict")
-def predict():
+@api.post("/predict")
+def predict(file: UploadFile = File(...)):
     return {"message": "Prediction endpoint"}   
 
